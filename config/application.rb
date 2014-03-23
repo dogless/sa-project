a54fafc8c56e5c1f91f8f47f7f7bcf9ab864902d
Doug <dougjchau@gmail.com>
2014-03-23 00:02:03 -0300

    fix precompile error

--- config/application.rb
+++ config/application.rb
@@ -58,5 +58,8 @@ module SaProject
 
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
+
+    #fix initial push
+    config.assets.initialize_on_precompile = false
   end
 end
