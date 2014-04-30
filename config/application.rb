de5ba4c3a562d0f5e26d128d4cfcdb8c2b68de87
Doug <dougjchau@gmail.com>
2014-04-30 17:57:10 -0300

    another application.rb fix

--- config/application.rb
+++ config/application.rb
@@ -55,7 +55,7 @@ module SaProject
 
     # Enable the asset pipeline
     config.assets.enabled = true
-    config.assets.precompile += ['index.css']
+    config.assets.compile = true
 
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
