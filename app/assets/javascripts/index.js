ccb157669c885064794e3ad15d5dab0c7f5eab48
Doug <dougjchau@gmail.com>
2014-05-06 23:50:35 -0300

    add bus15 kml

--- app/assets/javascripts/index.js
+++ app/assets/javascripts/index.js
@@ -5,5 +5,10 @@ handler.buildMap({
 			zoom: 15,
 			center: new google.maps.LatLng(42.088824, -75.966983) //binghamton coords
 		}
+	},
+	function(){
+		var kmls = handler.addKml(
+			{ url: "https://sites.google.com/site/dougjchau/Bus%2015.kml?attredirects=0&d=1"}
+		);
 	}
 );
