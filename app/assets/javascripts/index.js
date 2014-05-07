196f0bbd868b9ea318a30537cb45394764f012b5
Doug <dougjchau@gmail.com>
2014-05-06 22:30:14 -0300

    fix gmaps to focus on binghamton lattitude and longitude

--- app/assets/javascripts/index.js
+++ app/assets/javascripts/index.js
@@ -1,2 +1,9 @@
 handler = Gmaps.build('Google');
-handler.buildMap({ internal: {id: 'map' }});
+handler.buildMap({ 
+		internal: {id: 'map' },
+		provider: {
+			zoom: 15,
+			center: new google.maps.LatLng(42.088824, -75.966983) //binghamton coords
+		}
+	}
+);
