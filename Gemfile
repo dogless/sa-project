919d4becdf8d4ac1b8e7da99827cf3a87250cee4
Doug <dougjchau@gmail.com>
2014-05-09 18:56:13 -0300

    add autocomplete

--- Gemfile
+++ Gemfile
@@ -34,6 +34,8 @@ group :assets do
 end
 
 gem 'jquery-rails'
+gem 'jquery-ui-rails'
+gem 'gmaps-autocomplete-rails'
 gem 'haml'
 gem 'gmaps4rails'
 
--- app/assets/javascripts/index.js
+++ app/assets/javascripts/index.js
@@ -12,3 +12,18 @@ handler.buildMap({
 		);
 	}
 );
+
+/*
+jQuery(function() {
+	var completer;
+
+	completer = new GmapsCompleter({
+		inputField: '#gmaps-input-address',
+		errorField: '#gmaps-error'
+	});
+
+	completer.autoCompleteInit({
+		country: "us"
+	});
+});
+*/
